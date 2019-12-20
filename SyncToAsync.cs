df2180844b86d2834461869714ef1b7d29aed995
Tim van Elst <timvanelst@users.noreply.github.com>
2019-12-20 08:41:23 +0100

    Add files via upload

---
+++ SyncToAsync.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace SyncToAsync
+{
+    public class SyncToAsync
+    {
+        private readonly ILogger<SyncToAsync> logger;
+
+        public SyncToAsync(ILogger<SyncToAsync> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Method()
+        {
+            var task = Task.Run(async () =>
+                {
+                    try
+                    {
+                        await MethodAsync().ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        logger.LogError(e, $"Error in Async Method.");
+                        throw;
+                    }
+
+
+
+                    return true;
+                }
+            ).GetAwaiter().GetResult();
+        }
+
+        public async Task MethodAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
